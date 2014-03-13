18d5d44b429f0772a04d0ecce40b24ffcf043f68
Danny Yu <dannyyu92@gmail.com>
2014-03-13 10:09:58 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProductsApi::Application.config.secret_key_base = '854ae616c2381ac1bd7e6445994d0a3f00983966e34ecd1cd645888921ede63f70a96d3707698085b8ec2cd60511c16d94e411706c11189ee9547f59f2f5ad89'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProductsApi::Application.config.session_store :cookie_store, key: '_products_api_session'
