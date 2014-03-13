5bc3f008ea408687dfe0e31de881e223add3a753
Danny Yu <dannyyu92@gmail.com>
2014-03-13 11:55:43 -0400

    Remove subdomain (not available on heroku :( )

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ProductsApi::Application.routes.draw do
 
-  namespace :api, defaults: { :format => :json }, path: "", constraints: { subdomain: "api" } do
+  namespace :api, defaults: { :format => :json } do
     namespace :v1 do
       resources :products
     end
